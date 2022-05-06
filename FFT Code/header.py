b8f916b0d463cfd5538d721dcde22ba8ae2e89f4
Atikah <atikah.hussain@hotmail.co.uk>
2022-05-06 23:56:34 +0100

    Add comments

--- FFT Code/header.py
+++ FFT Code/header.py
@@ -1,12 +1,12 @@
-# fft.py, ifft.py, dft.py
+# for fft.py, ifft.py, dft.py
 from math import cos, sin, pi, exp, log, ceil
 
-# test_runtime.py
+# for test_runtime.py
 from time import time
 from random import randint
 from tabulate import tabulate
 
-# test_inbuilt_fft.py
+# for test_inbuilt_fft.py
 import numpy as np
 from scipy.fft import fft as scipy_fft
 from scipy.fft import ifft as scipy_ifft
