e042c4e052d4c8f5d42c92ec20f98cc0f92d2585
Atikah <atikah.hussain@hotmail.co.uk>
2022-05-06 23:56:26 +0100

    Test fft polynomial multiplication on a manual example

--- FFT Code/fft_polynomial_multiplication.py
+++ FFT Code/fft_polynomial_multiplication.py
@@ -14,8 +14,9 @@ def FFT_multiplication(A_coeff, B_coeff):
     return C_coeff
 
 if __name__ == '__main__':
-    A = [2, 3, 7]
-    B = [1, 0, 2]
-    # expected output from naive multiplication: [2, 3, 11, 6, 14]
+    # A = [2, 3, 7]
+    # B = [1, 0, 2] # expected output from naive multiplication: [2, 3, 11, 6, 14]
+    A = [1, 0, 1]
+    B = [0, 3, 4] # expected output from naive multiplication: [3, 4, 3, 4]
     C = FFT_multiplication(A, B)
     print(C)
