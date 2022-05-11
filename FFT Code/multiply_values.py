a1299bff8b66a54fa5e0f3e730720309fd1febbf
Atikah <atikah.hussain@hotmail.co.uk>
2022-05-11 12:27:25 +0100

    Fix comments and docstring

--- FFT Code/multiply_values.py
+++ FFT Code/multiply_values.py
@@ -1,10 +1,6 @@
-
-
 def multiply_values(A, B):
-    """
-    The function takes two arrays, length n, of complex numbers, and carries out an element-wise multiplication.
-    The function returns the product as an array of length n.
-    """
+    """A and B are arrays of complex numbers.
+    Carries out an element-wise multiplication, returns the product as an array"""
     C = [0 for _ in range(len(A))]
     for i in range(len(C)):
         C[i] = A[i]*B[i]
@@ -13,5 +9,7 @@ def multiply_values(A, B):
 if __name__ == '__main__':
     A = [(1+0j), 1j, (-1+0j), (-0-1j)]
     B = [(1+0j), 1j, (-1+0j), (-0-1j)]
+    # A = [1, 0, 1]
+    # B = [3, 4] # to check padding..
     C = multiply_values(A, B)
     print(C)
