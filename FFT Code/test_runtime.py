d23fff40370ba1ac54dab77d04fc925c6b51aa33
Atikah <atikah.hussain@hotmail.co.uk>
2022-05-11 12:31:19 +0100

    Adjust test_runtime function's implementation

--- FFT Code/test_runtime.py
+++ FFT Code/test_runtime.py
@@ -3,10 +3,15 @@ from naive_polynomial_multiplication import naive_multiplication
 from fft_polynomial_multiplication import FFT_multiplication
 
 def test_timing(f):
-    n = [10, 100, 1000] # input size
-    timings = []
+    """For an algorithm defined as a function, f,
 
-    for i in range(len(n)):
+    k = list(range(5, 15)) # range of input sizes to use input sizes
+    n = [2**i for i in k] # all input sizes in an array
+    number_of_tests = len(n)
+    timings = [] # store the timings
+
+    for i in range(number_of_tests):
+        # Generate two polynomials of random coefficients
         A = [randint(0, 999) for _ in range(n[i])]
         B = [randint(0, 999) for _ in range(n[i])]
 
@@ -19,6 +24,7 @@ def test_timing(f):
     print(f"{f.__name__.replace('_', ' ')}:")
     print(tabulate(table, headers="keys"))
 
+    return timings, n, k
 
 if __name__ == '__main__':
     test_timing(naive_multiplication)
