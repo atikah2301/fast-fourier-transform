1b62f4356237dd509351dd3348761de34b3d0967
Atikah <atikah.hussain@hotmail.co.uk>
2022-05-11 12:33:13 +0100

    Add docstrings

--- FFT Code/rounding.py
+++ FFT Code/rounding.py
@@ -1,17 +1,21 @@
 def round_complex(number, decimal=5):
+    """number: complex number, decimal: int"""
     return complex(round(number.real, decimal), round(number.imag, decimal))
 
 
 def round_complex_nums(numbers, decimal=5):
+    """numbers: array of complex numbers, decimal: int"""
     return [round_complex(number, decimal=decimal) for number in numbers]
 
 
 def round_real_nums(numbers, decimal=5):
+    """numbers: array of real numbers, decimal: int"""
     return [round(number, decimal) for number in numbers]
 
 
 def round_nums(numbers, decimal=5):
     """
+    numbers: array of numbers, decimal: int
     Only use if input is known to be either all complex or all real, but unsure which
     """
     if isinstance(numbers[0], complex):
