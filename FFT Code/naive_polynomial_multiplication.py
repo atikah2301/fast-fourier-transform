b8f9946707c48d70f92748208a572e34886bc85b
Atikah <atikah.hussain@hotmail.co.uk>
2022-05-11 12:34:00 +0100

    Shorten code and adjust comments

--- FFT Code/naive_polynomial_multiplication.py
+++ FFT Code/naive_polynomial_multiplication.py
@@ -1,16 +1,12 @@
 def naive_multiplication(A, B):
     # Lengths & degrees of A(x) and B(x)
-    n_A = len(A)
-    n_B = len(B)
-    d_A = n_A - 1
-    d_B = n_B - 1
+    n_A = len(A); n_B = len(B)
+    d_A = n_A - 1; d_B = n_B - 1
 
     # Degree & length of C(x), the product
-    d_C = d_A + d_B
-    n_C = d_C + 1
+    d_C = d_A + d_B; n_C = d_C + 1
 
     C = [0 for i in range(n_C)]
-
     # Calculate new coefficients as the sum of pair-wise products of old coefficients
     for i in range(n_A):
         for j in range(n_B):
@@ -21,6 +17,6 @@ def naive_multiplication(A, B):
 if __name__ == '__main__':
     A = [2, 3, 7]
     B = [1, 0, 2]
-    # expected output, from manual calculation: [2, 3, 11, 6, 14]
+    # output as expected, from manual calculation: [2, 3, 11, 6, 14]
     C = naive_multiplication(A, B)
     print(C)
